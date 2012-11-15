d068994c3e5f8aa6432d5ecb53bf0d0018c959ac
Giorgi Kandelaki <kandelakig@gmail.com>
2012-11-16 00:27:38 +0400

    First implementation

---
+++ js/promises.js
@@ -0,0 +1,74 @@
+function defer() {
+  var handlers = [];
+
+  var value;
+  var error;
+
+  return {
+    promise: {
+      then: function(callback, errback) {
+        var d = defer()
+        
+        if (value) {
+          if (callback) {
+            try {
+              d.resolve(callback(value));
+            } catch (e) {
+              d.reject(e);
+            }
+          } else {
+            d.resolve(value);
+          }
+        } else if (error) {
+          if (errback) {
+            try {
+              d.resolve(errback(error));
+            } catch (e) {
+              d.reject(e);
+            }
+          } else {
+            d.reject(e);
+          }
+        } else {
+          handlers.push({
+            resolved: callback,
+            rejected: errback,
+            defered: d
+          })
+        }
+
+        return d.promise;
+      }
+    },
+
+    resolve: function(val) {
+      value = val;
+      handlers.forEach(function(handler){
+        try {
+          if (handler.resolved) {
+            handler.defered.resolve(handler.resolved(val));
+          } else {
+            handler.defered.resolve(val);
+          }
+        } catch (e) {
+          handler.defered.reject(e);
+        }
+      })
+    },
+
+    reject: function(err) {
+      error = err;
+      handlers.forEach(function(handler){
+        try {
+          if (handler.rejected) {
+            handler.defered.resolve(handler.rejected(val));
+          } else {
+            handler.defered.reject(val);
+          }
+        } catch (e) {
+          handler.defered.reject(e);
+        }
+      })
+    }
+  }
+}
